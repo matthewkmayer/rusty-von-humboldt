6fef6d8de9321713bbbd30856a56d228182cf577
Matthew Mayer <matthewkmayer@gmail.com>
2017-11-18 16:54:00 -0800

    Add timing and eliminate some work we were doing.

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,13 @@ extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
 extern crate rayon;
+extern crate stopwatch;
 
+use stopwatch::Stopwatch;
 use clap::App;
 use std::fs::File;
 use std::io::prelude::*;
+use std::io::BufReader;
 use rayon::prelude::*;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -42,24 +45,26 @@ fn main() {
 
     // Hardcoded path to a github archive file for now
     let sample_file = "../2017-01-01-15.json";
-    let mut f = File::open(sample_file).expect("file not found");
+    let f = File::open(sample_file).expect("file not found");
 
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
-
+    let mut sw = Stopwatch::start_new();
     // temp_stringy only present since I can't get a par_iter directly from .split()
-    let mut temp_stringy = Vec::with_capacity(25000);
-    for line in contents.split("\n") {
-        if line.len() > 0 {
-            temp_stringy.push(line);
+    let mut temp_stringy: Vec<String> = Vec::with_capacity(25000);
+    for line in BufReader::new(f).lines() {
+        match line {
+            Ok(l) => temp_stringy.push(l),
+            Err(_) => (),
         }
     }
+    println!("file reading fun took {}ms", sw.elapsed_ms());
 
+    sw.restart();
     let events: Vec<Event> = temp_stringy
         .par_iter()
         .map(|l| serde_json::from_str(&l).expect("Couldn't deserialize event file."))
         .collect();
+
+    println!("Deserialization took {}ms", sw.elapsed_ms());
 
     // display something interesting
     println!("\nFound {} events", events.len());
